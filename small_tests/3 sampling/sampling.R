a7756e6fd93586b50b825ee5203c1c13beb296b0
bsmg1h <438283168@qq.com>
2017-03-17 20:03:53 +0800

    3.17 second

--- small_tests/3 sampling/sampling.R
+++ small_tests/3 sampling/sampling.R
@@ -6,12 +6,30 @@ colorbar <- c("#fdbb84","#de2d26","#FFFFFF")
 
 # 1. Load network and log ----------------------------------------------------------------
 wd = getwd()
-data <- read.csv(paste(wd,"small_tests","3 sampling","log.csv",sep = "/"))
+log <- read.csv(paste(wd,"small_tests","3 sampling","log.csv",sep = "/"))
 load("model")
 g = network$g
+pb_matrix = network$pb_matrix
 
 # 2. Sampling -------------------------------------------------------------
 
-Sampling <- function(passageID = sample(1:300,1), log, )
+Positive_Sampling <- function(passageID = sample(1:300,1), log, g, pb_matrix){
+	diffusion_result = log %>% filter(passageID == passageID)
+	n_actived_point = nrow(diffusion_result)
+	if(actived_point < 20){
+		return()
+	} 
+	sample = diffusion_result[sample(1:n_actived_point,1),]
+	if(sample[2] <= 2){
+		return()
+	}
+	t = sample[2] - 1
+	output = list(passageID = passageID,
+								sample_point = sample[1],
+								t=t)
+}
 
 
+Negetive_Sampling <- function(passageID = sample(1:300,1), log, g, pb_matrix){
+	diffusion_result = log %>% filter(passageID == passageID)
+}
